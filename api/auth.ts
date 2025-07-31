cac9b43c5e68a68e226f52403e86e59b978ec861
Juhkang <juhyunk0820@naver.com>
2025-08-01 00:18:55 +0900

    로그인 api 및 정보 조회 api

--- api/auth.ts
+++ api/auth.ts
@@ -1,9 +1,26 @@
+import { UserInfo } from "@/types/domain/user";
 import { axiosInstance } from "./axios";
+import { RequestLogin } from "@/types/request/auth";
+import { ResponseLogin } from "@/types/response/auth";
 
-async function kakaoLogin() {
-  const { data } = await axiosInstance.post("");
+async function postLogin({
+  OauthId,
+  name,
+  nickname,
+  email,
+  gender,
+  birth,
+}: RequestLogin): Promise<ResponseLogin> {
+  const { data } = await axiosInstance.post("/signup/register");
 
   return data;
 }
 
-export { kakaoLogin };
+async function getMyInfo(): Promise<UserInfo> {
+  //토큰 첨부 필요?
+  const { data } = await axiosInstance.get("/signup/register");
+
+  return data;
+}
+
+export { postLogin, getMyInfo };
