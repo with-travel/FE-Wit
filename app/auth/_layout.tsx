33f02cafbdedb3da280e269dec789a702a3ed2e4
Juhkang <juhyunk0820@naver.com>
2025-07-31 23:53:19 +0900

    auth - travelFormScreen 요소 추가

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,14 +1,43 @@
 import { Stack } from "expo-router";
+import { FormProvider, useForm } from "react-hook-form";
+
+type AuthForm = {
+  oAuthId: string;
+  name: string;
+  nickname: string;
+  email: string;
+  gender: string;
+  birth: string;
+};
 
 export default function AuthLayout() {
+  const form = useForm<AuthForm>({
+    defaultValues: {
+      oAuthId: "",
+      name: "",
+      nickname: "",
+      email: "",
+      gender: "",
+      birth: "",
+    },
+  });
+
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <FormProvider {...form}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="travelform"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </FormProvider>
   );
 }
