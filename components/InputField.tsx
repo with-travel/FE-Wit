553f8f364d5b25c501df7639784e1a810ceb62aa
Juhkang <juhyunk0820@naver.com>
2025-08-11 16:34:27 +0900

    커스텀 입력 컴포넌트

---
+++ components/InputField.tsx
@@ -0,0 +1,47 @@
+import { colors } from "@/constants/colors";
+import React from "react";
+import {
+  StyleSheet,
+  TextInputProps,
+  View,
+  Text,
+  TextInput,
+} from "react-native";
+
+interface InputFieldProps extends TextInputProps {
+  label: string;
+  error?: string;
+}
+
+function InputField({ label, error, ...props }: InputFieldProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.labelText}>{label}</Text>
+      <TextInput {...props} style={styles.inputContainer} />
+      {error && <Text style={styles.error}>{error}</Text>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 32,
+  },
+  labelText: {
+    marginBottom: 4,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    borderColor: colors.PRIMARY_COLOR,
+    borderRadius: 5,
+    height: 50,
+    backgroundColor: "#F3F3F3",
+    color: colors.GRAY_400,
+  },
+  error: {
+    color: colors.RED_500,
+  },
+});
+
+export default InputField;
